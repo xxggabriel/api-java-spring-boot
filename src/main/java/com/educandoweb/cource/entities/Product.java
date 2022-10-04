30386ef13ec58e95698ffa4979f1360b5fe77dfe
Gabriel Oliveira <gabriel@umobi.com.br>
2022-10-04 10:05:34 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/cource/entities/Product.java
+++ src/main/java/com/educandoweb/cource/entities/Product.java
@@ -28,6 +28,9 @@ public class Product implements Serializable {
     )
     private Set<Category> categories =  new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
    public Product(){
 
    }
@@ -82,6 +85,17 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+       Set<Order> set = new HashSet<>();
+
+       for (OrderItem x : items) {
+           set.add(x.getOrder());
+       }
+
+       return set;
     }
 
     @Override
